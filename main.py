2fb3e6a9e912ab664c49b5f982b51c3abd69e655
Dawn <marehazydawn@outlook.com>
2025-09-27 01:05:17 +0930

    arithmetic operators

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-# Madlibs game
-# word game where you create a story
-# by filling in blanks with random words
+friends = 10
 
-adjective1 = input("Enter an adjective (description): ")
-noun1 = input("Enter a noun (person, place, thing): ")
-adjective2 = input("Enter an adjective (description): ")
-verb1 = input("Enter averb ending with 'ing': ")
-adjective3 = input("Enter an adjective (description): ")
+# friends = friends + 1
+# friends += 1
+# friends = friends - 2
+# friends -= 2
+# friends = friends * 3
+# friends *= 3
+# friends = friends / 2
+# friends /= 2
+# friends = friends ** 2
+# friends **= 2
+# remainder = friends % 2
 
-print(f"Today I went to a {adjective1} zoo.")
-print(f"In an exhibit, I saw a {noun1}")
-print(f"{noun1} was {adjective2} and {verb1}")
-print(f"I was {adjective3}!")
+print(friends)
