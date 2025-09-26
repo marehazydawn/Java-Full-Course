5c4ba69e967f925e13875fc1e9e00ec7c43c12f8
Dawn <marehazydawn@outlook.com>
2025-09-27 01:20:57 +0930

    math funcs 2

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
+import math
 
-x = 3.14
-y = 4
-z = 5
+x = 9
 
-# result = round(x)
-# result = abs(y)
-# result = pow(4, y)
-# result = max(x, y, z)
-result = min(x, y, z)
+# print(math.pi)
+# print(math.e)
+# result = math.sqrt(x)
+# result = math.ceil(x)
+result = math.floor(x)
 
 print(result)
