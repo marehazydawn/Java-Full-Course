0bdebd03357c70ed4f5cc53ee5112411dd200efc
Dawn <marehazydawn@outlook.com>
2025-09-27 01:13:06 +0930

    math funcs

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-friends = 10
 
-# friends = friends + 1
-# friends += 1
-# friends = friends - 2
-# friends -= 2
-# friends = friends * 3
-# friends *= 3
-# friends = friends / 2
-# friends /= 2
-# friends = friends ** 2
-# friends **= 2
-# remainder = friends % 2
+x = 3.14
+y = 4
+z = 5
 
-print(friends)
+# result = round(x)
+# result = abs(y)
+# result = pow(4, y)
+# result = max(x, y, z)
+result = min(x, y, z)
+
+print(result)
