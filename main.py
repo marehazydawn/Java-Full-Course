c7b24018dceb1dfd5c0050171156ca373d51f152
Dawn <marehazydawn@outlook.com>
2025-09-26 16:57:33 +0930

    print f-string integer

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ first_name = "Sis"
 food = "pizza"
 email = "some@email.com"
 
-print(f"Hello {first_name}")
-print(f"You like {food}")
-print(f"Your email is: {email}")
+#Integers
+age = 29
+quantity = 3
+num_of_strudents = 30
+
+print(f"you are {age} years old")
+print(f"You are buying {quantity} items")
+print(f"Your class has {num_of_strudents} students")
