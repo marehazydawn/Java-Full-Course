cb30b78677d95a8c36681ce2da13fb08c65128a7
Dawn <marehazydawn@outlook.com>
2025-09-27 21:51:09 +0930

    math funcs 4 hypotenuse

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import math
 
-radius = float(input('enter the radius of a circle: '))
+a = float(input("Enter side A: "))
+b = float(input("Enter side B: "))
 
-# circumference = 2 * math.pi * radius
-# print(f"The circumference is: {round(circumference, 2)} cm")
+c = math.sqrt(pow(a, 2) + pow(b, 2))
 
-area = math.pi * pow(radius, 2)
-print(f"The area of the circle is: {round(area, 2)}cm^2")
+print(f"Side X = {c}")
