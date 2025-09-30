decc1ae794c1401e3a6ba3311ac5f397b3bd869c
Dawn <marehazydawn@outlook.com>
2025-09-30 20:03:54 +0930

    string methods

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
-# conditional expression = A one-liner shortcut for the if-else statement (ternary  operator)
-#                          Print or assign one of the two values based on condition
-#                          X if condition else Y
 
-num = 5
-a = 6
-b = 7
-age = 13
-temperature = 30
-user_role = "admin"
+# print(help(str))
 
-# print("Positive" if num > 0 else "Negative")
-# result = "EVEN" if num % 2 == 0 else "ODD"
-# max_num = a if a > b else b
-# min_num = a if a < b else b
-# status = "ADULT" if age >= 18 else "Child"
-#w eather = "HOT" if temperature > 20 else "COLD"
-access_level = "FULL Access" if user_role == "admin" else "Limited Access"
+# name = input("Enter your full name: ")
+phone_number = input("Enter your phone number: ")
 
-print(access_level)
+# result = len(name)
+# result = name.find("D")
+# result = name.rfind("n")
+# name = name.capitalize()
+# name = name.upper()
+# name = name.lower()
+# result = name.isdigit()
+# result = name.isalpha()
+# result = phone_number.count("-")
+phone_number = phone_number.replace("-", "")
+
+print(phone_number)
