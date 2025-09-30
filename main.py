f8ae594ea9f6e244dad81d5bef855ffa4150e71b
Dawn <marehazydawn@outlook.com>
2025-09-30 20:18:16 +0930

    indexing

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# valid user input excercise
-# 1. username is no more than 12 characters
-# 2. username must not contain spaces
-# 3. username must not contain digits
+# indexing = accessing elements of a sequence using [] (indexing operators)
+#            [Start : end : step]
 
-username = input("Enter a username: ")
+credit_number = "1234-5678-9012-3456"
 
-
-if len(username) > 12:
-    print("Your username can't be more than 12 characters")
-elif not username.find(" ") == -1:
-    print("Your username can't contain spaces")
-elif not username.isalpha():
-    print("Your username can't contain numbers")
-else:
-    print(f"Welcome {username}")
+# print(credit_number[4])
+# print(credit_number[0:4])
+# print(credit_number[5:9])
+# print(credit_number[5:])
+# print(credit_number[-1])
+# print(credit_number[::2])
