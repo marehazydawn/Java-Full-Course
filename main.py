8004b35dfeab2bed92b4b11d9da8c4d6b655d6c0
Dawn <marehazydawn@outlook.com>
2025-09-30 20:12:13 +0930

    string methods 2

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
+# valid user input excercise
+# 1. username is no more than 12 characters
+# 2. username must not contain spaces
+# 3. username must not contain digits
 
-# print(help(str))
+username = input("Enter a username: ")
 
-# name = input("Enter your full name: ")
-phone_number = input("Enter your phone number: ")
 
-# result = len(name)
-# result = name.find("D")
-# result = name.rfind("n")
-# name = name.capitalize()
-# name = name.upper()
-# name = name.lower()
-# result = name.isdigit()
-# result = name.isalpha()
-# result = phone_number.count("-")
-phone_number = phone_number.replace("-", "")
-
-print(phone_number)
+if len(username) > 12:
+    print("Your username can't be more than 12 characters")
+elif not username.find(" ") == -1:
+    print("Your username can't contain spaces")
+elif not username.isalpha():
+    print("Your username can't contain numbers")
+else:
+    print(f"Welcome {username}")
