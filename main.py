1ed2f87f31b3433abffc62915922537a1771bd79
Dawn <marehazydawn@outlook.com>
2025-09-30 19:47:51 +0930

    logical operators 2

--- main.py
+++ main.py
@@ -3,10 +3,25 @@
 #                     and = both conditions must be True
 #                     not = inverts the condition (not False, not True)
 
-temp = 25
-is_raining = False
+temp = 28
+is_sunny = False
 
-if temp > 35 or temp < 0 or is_raining:
-    print("The outdoor event is cancelled")
-else:
-    print("The outdoor event is still cancelled")
+if temp >= 28 and is_sunny:
+    print("It is hot outside")
+    print("It is sunny")
+elif temp <= 0 and is_sunny:
+    print("It is cold outside")
+    print("It is sunny")
+elif temp < 28 and temp > 0 and is_sunny:
+    print("It is warm outside")
+    print("It is sunny")
+
+elif temp >= 28 and not is_sunny:
+    print("It is hot outside")
+    print("It is cloudy")
+elif temp <= 0 and not is_sunny:
+    print("It is cold outside")
+    print("It is cloudy")
+elif temp < 28 and temp > 0 and not is_sunny:
+    print("It is warm outside")
+    print("It is cloudy")
