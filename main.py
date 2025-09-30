efa8a1cf3f73fc74a915e28cf7bb92a7fc895128
Dawn <marehazydawn@outlook.com>
2025-09-30 19:54:34 +0930

    conditional expressions

--- main.py
+++ main.py
@@ -1,27 +1,20 @@
-# logical operators = evaluate multiple conditions (or, and, not)
-#                     or = at lease one condition must be True
-#                     and = both conditions must be True
-#                     not = inverts the condition (not False, not True)
+# conditional expression = A one-liner shortcut for the if-else statement (ternary  operator)
+#                          Print or assign one of the two values based on condition
+#                          X if condition else Y
 
-temp = 28
-is_sunny = False
+num = 5
+a = 6
+b = 7
+age = 13
+temperature = 30
+user_role = "admin"
 
-if temp >= 28 and is_sunny:
-    print("It is hot outside")
-    print("It is sunny")
-elif temp <= 0 and is_sunny:
-    print("It is cold outside")
-    print("It is sunny")
-elif temp < 28 and temp > 0 and is_sunny:
-    print("It is warm outside")
-    print("It is sunny")
+# print("Positive" if num > 0 else "Negative")
+# result = "EVEN" if num % 2 == 0 else "ODD"
+# max_num = a if a > b else b
+# min_num = a if a < b else b
+# status = "ADULT" if age >= 18 else "Child"
+#w eather = "HOT" if temperature > 20 else "COLD"
+access_level = "FULL Access" if user_role == "admin" else "Limited Access"
 
-elif temp >= 28 and not is_sunny:
-    print("It is hot outside")
-    print("It is cloudy")
-elif temp <= 0 and not is_sunny:
-    print("It is cold outside")
-    print("It is cloudy")
-elif temp < 28 and temp > 0 and not is_sunny:
-    print("It is warm outside")
-    print("It is cloudy")
+print(access_level)
